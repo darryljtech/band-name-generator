25bae9b43e566123efc9c805f6c977c66b49aac0
darryljtech <darryl.avery.tech@gmail.com>
2023-08-07 12:19:36 -0400

    Adding a newline after the input

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ print("Hello! Welcome to Band Name Generator !")
 
 # Ask the user for the city that they grew up in
 
-birth_city_input = input("\nWhat city did you grow up in?: ")
+birth_city_input = input("What city did you grow up in?: \n")
 
 # Ask the user for the name of a pet
 
-favorite_pet_input = input("\nWhat is the name of your favorite pet?: ")
+favorite_pet_input = input("\nWhat is the name of your favorite pet?: \n")
 
 band_name = birth_city_input + " " + favorite_pet_input
 
